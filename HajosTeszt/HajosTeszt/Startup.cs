e7b01957cb755e3f0461cc0030458efbb0936ffa
corvinus-rv5qb9 <gergely.vidor@stud.uni-corvinus.hu>
2021-03-19 17:43:37 +0100

    jsgyak ready

--- HajosTeszt/HajosTeszt/Startup.cs
+++ HajosTeszt/HajosTeszt/Startup.cs
@@ -30,7 +30,7 @@ namespace HajosTeszt
 
             DefaultFilesOptions options = new DefaultFilesOptions();
             options.DefaultFileNames.Clear();
-            options.DefaultFileNames.Add("hajo.html");
+            options.DefaultFileNames.Add("jsgyak.html");
             app.UseDefaultFiles(options);
             app.UseStaticFiles();
 

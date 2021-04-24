49e20e7106a88935645c16b2a2eae54388b1a374
corvinus-rv5qb9 <gergely.vidor@stud.uni-corvinus.hu>
2021-04-24 14:04:43 +0200

    9. gyak

--- HajosTeszt/HajosTeszt/wwwroot/hetedik.js
+++ HajosTeszt/HajosTeszt/wwwroot/hetedik.js
@@ -58,3 +58,60 @@ színezés = function (n) {
     else { document.getElementById("válasz" + n).style.backgroundColor = "red" }
 }
 
+function kérdésMegjelenítés(kérdés) {
+    console.log(kérdés);
+    document.getElementById("kérdés_szöveg").innerText = kérdés.questionText
+    document.getElementById("válasz1").innerText = kérdés.answer1
+    document.getElementById("válasz2").innerText = kérdés.answer2
+    document.getElementById("válasz3").innerText = kérdés.answer3
+    document.getElementById("kép").src = "https://szoft1.comeback.hu/hajo/" + kérdés.image;
+}
+
+function kérdésBetöltés(id) {
+    fetch(`/questions/${id}`)
+        .then(response => {
+            if (!response.ok) {
+                console.error(`Hibás válasz: ${response.status}`)
+            }
+            else {
+                return response.json()
+            }
+        })
+        .then(data => kérdésMegjelenítés(data));
+}
+
+function válaszfeldolgozás(válasz) {
+    if (!válasz.ok) {
+        console.error(`Hibás válasz: ${response.status}`)
+    }
+    else {
+        return válasz.json()
+    }
+}
+
+function kiiratás(lista) {
+    console.log(lista)
+    for (var i = 0; i < lista.length; i++) {
+        let újElem = document.createElement("div");
+        újElem.innerText = lista[i];
+        document.getElementById("eredmeny").appendChild(újElem);
+    }
+}
+
+fetch('/questions/all')
+    .then(response => response.json())
+    .then(data => console.log(data)
+    );
+
+
+
+fetch('/questions/4')
+    .then(response => response.json())
+    .then(data => console.log(data)
+    );
+
+
+fetch('/questions/1')
+    .then(response => response.json())
+    .then(data => kérdésMegjelenítés(data)
+    );
